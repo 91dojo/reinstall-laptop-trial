1134ce097ec1d0995c9264c6d2a316d2d4a45658
Joey Chen <jay.hatelove@gmail.com>
2018-08-25 14:17:30 +0800

    test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TennisTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
